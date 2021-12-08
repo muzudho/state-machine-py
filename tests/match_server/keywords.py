e1b592d6fa09aa972dedd6dbafed01e5cfc9e1c5
むずでょ <muzudho1@gmail.com>
2021-12-08 20:53:46 +0900

    match_server WIP

---
+++ tests/match_server/keywords.py
@@ -0,0 +1,27 @@
+# Machines
+
+MACHINE_C = "[[MachineC]]"
+MACHINE_S = "[[MachineS]]"
+
+# States
+
+INIT = "[Init]"
+LOBBY = "[Lobby]"
+REPLY = "[Reply]"
+GAME = "[Game]"
+
+# Edges
+
+E_LOGIN = "-Login->"
+E_OK = "-Ok->"
+E_INCORRECT = "-Incorrect->"
+E_GAME_SUMMARY = "-GameSummary->"
+E_END_GAME_SUMMARY = "-EndGameSummary->"
+E_LOGOUT = "-Logout->"
+E_COMPLETED = "-Completed->"
+E_AGREE = "-Agree->"
+E_START = "-Start->"
+E_REJECT = "-Reject->"
+E_MOVE = "-Move->"
+E_MOVE_ECHO = "-MoveEcho->"
+E_GAME_OVER = "-GameOver->"
---
+++ tests/match_server/machine_c/layer1/init.py
@@ -0,0 +1,47 @@
+import time
+from state_machine_py.abstract_state import AbstractState
+from tests.match_server.keywords import E_LOGIN, INIT
+
+
+class InitState(AbstractState):
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def name(self):
+        return INIT
+
+    def exit(self, req):
+        """次の辺の名前を返します
+
+        Parameters
+        ----------
+        req : Request
+            ステートマシンからステートへ与えられる引数のまとまり
+
+        Returns
+        -------
+        str
+            辺の名前
+        """
+
+        edge_path = "/".join(req.edge_path)
+
+        if edge_path == "":
+            self.on_login(req)
+            return E_LOGIN
+
+        elif edge_path == f"{E_LOGIN}":
+            # TODO サーバーからの Ok か Incorrect かのメッセージを待っています
+            print("サーバーからの Ok か Incorrect かのメッセージを待っています")
+            while True:
+                # TODO ここでキューからメッセージを取得したい
+                time.sleep(3)
+
+        else:
+            raise ValueError(f"Invalid edge_path={edge_path}")
+
+    def on_login(self, req):
+        """ログイン時"""
+        pass
