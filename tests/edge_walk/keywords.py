f7ac46df74b4f7eca7da72f70d2deb59f050c010
むずでょ <muzudho1@gmail.com>
2021-12-09 22:00:39 +0900

    Edge

--- tests/edge_walk/keywords.py
+++ tests/edge_walk/keywords.py
@@ -6,3 +6,16 @@ MACHINE_A = "[[MachineA]]"
 
 INIT = "[Init]"
 GOAL = "[Goal]"
+
+# Edges
+
+E_LOOPBACK = ""
+E_THAT = "-that->"
+E_THIS = "-this->"
+E_WAS = "-was->"
+E_IS = "-is->"
+E_AN = "-an->"
+E_A = "-a->"
+E_PIN = "-pin->"
+E_PEN = "-pen->"
+E_OK = "-ok->"
--- tests/edge_walk/transition_dict.py
+++ tests/edge_walk/transition_dict.py
@@ -1,15 +1,4 @@
-from tests.edge_walk.keywords import INIT, GOAL
-
-
-E_THAT = "-that->"
-E_THIS = "-this->"
-E_WAS = "-was->"
-E_IS = "-is->"
-E_AN = "-an->"
-E_A = "-a->"
-E_PIN = "-pin->"
-E_PEN = "-pen->"
-E_OK = "-ok->"
+from tests.edge_walk.keywords import E_A, E_AN, E_IS, E_OK, E_PEN, E_PIN, E_THAT, E_THIS, E_WAS, INIT, GOAL
 
 transition_dict = {
     INIT: {
