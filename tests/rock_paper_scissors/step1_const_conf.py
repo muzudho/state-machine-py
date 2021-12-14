15d78cebb1831d39a6b3a0fddbc3287bb04c19b1
むずでょ <muzudho1@gmail.com>
2021-12-14 21:25:28 +0900

    Update step1_const_conf.py

--- tests/rock_paper_scissors/step1_const_conf.py
+++ tests/rock_paper_scissors/step1_const_conf.py
@@ -1,15 +1,26 @@
 # State machines
+# --------------
 
 MACHINE_A = "MachineA"
 
 # States
+# ------
+#
+# "ステート定数": "ステートマシン図のノード名"
+# 本レッスンではノード名を PascalCase にします（エッジと被らないように）
+
 INIT = "Init"
 GAME = "Game"
 
 # Edges
+# -----
+#
+# "エッジ定数": "ステートマシン図のエッジ名"
+# 本レッスンではエッジ名を snake_case にします（ノード名と被らないように）
+# ディクショナリーのキーとして State と被らないように頭に E_ を付けます
 
-E_LOOPBACK = "Loopback"  # 汎用
-E_LOGIN = "Login"
-E_LOSE = "Lose"
-E_WIN = "Win"
-E_DRAW = "Draw"
+E_LOOPBACK = "loopback"  # 汎用
+E_LOGIN = "login"
+E_LOSE = "lose"
+E_WIN = "win"
+E_DRAW = "draw"
