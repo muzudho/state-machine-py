ae466fa87527d384d36424e59688fc7cc5a47814
むずでょ <muzudho1@gmail.com>
2022-01-15 13:13:12 +0900

    TMC

--- tests/two_machines_catchball/main.py
+++ tests/two_machines_catchball/main.py
@@ -2,56 +2,49 @@ import sys
 import signal
 import argparse
 
+from state_machine_py.main_finally import MainFinally
 from tests.two_machines_catchball.main_diagram import MainDiagram
 from state_machine_py.code_gen.toml_reader_v11n90 import TomlReaderV11n90
 from state_machine_py.code_gen.json_reader_v11n100 import JsonReaderV11n100
 
 
-def __main():
-    """定型処理
-    この関数はこの形にし、 MainDiagram クラスの方に処理を書いてください"""
-    parser = argparse.ArgumentParser(description='設定ファイルを読み込みます')
-    parser.add_argument('conf', help='設定ファイルへのパス')
-    args = parser.parse_args()
+class Main():
+    def __init__(self):
+        self.__diagram = None
 
-    # 設定ファイル（.toml）読取
-    toml_doc = TomlReaderV11n90.read_file(args.conf)
+    def on_main(self):
+        """ここで通常の処理
+        この関数はこの形にし、 MainDiagram クラスの方に処理を書いてください"""
+        parser = argparse.ArgumentParser(description='設定ファイルを読み込みます')
+        parser.add_argument('conf', help='設定ファイルへのパス')
+        args = parser.parse_args()
 
-    # TOMLの内容を読み取ります
-    machinea_transition_file = toml_doc['machinea_transition_file']
-    machineb_transition_file = toml_doc['machineb_transition_file']
+        # 設定ファイル（.toml）読取
+        toml_doc = TomlReaderV11n90.read_file(args.conf)
 
-    # JSONファイルを読込みます
-    machinea_transition_doc = JsonReaderV11n100.read_file(
-        machinea_transition_file)
-    machineb_transition_doc = JsonReaderV11n100.read_file(
-        machineb_transition_file)
+        # TOMLの内容を読み取ります
+        machinea_transition_file = toml_doc['machinea_transition_file']
+        machineb_transition_file = toml_doc['machineb_transition_file']
 
-    def sigterm_handler(_signum, _frame) -> None:
-        sys.exit(1)
+        # JSONファイルを読込みます
+        machinea_transition_doc = JsonReaderV11n100.read_file(
+            machinea_transition_file)
+        machineb_transition_doc = JsonReaderV11n100.read_file(
+            machineb_transition_file)
 
-    # 強制終了のシグナルを受け取ったら、強制終了するようにします
-    signal.signal(signal.SIGTERM, sigterm_handler)
-
-    # ダイアグラムの初期化
-    example_diagram = MainDiagram(
-        machinea_transition_doc, machineb_transition_doc)
-    example_diagram.set_up()
-
-    try:
+        # ダイアグラムの初期化
+        self.__diagram = MainDiagram(
+            machinea_transition_doc, machineb_transition_doc)
+        self.__diagram.set_up()
         # ダイアグラムの実行
-        example_diagram.run()
+        self.__diagram.run()
 
-    finally:
-        # 強制終了のシグナルを無視するようにしてから、クリーンアップ処理へ進みます
-        signal.signal(signal.SIGTERM, signal.SIG_IGN)
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-        example_diagram.clean_up()
-        # 強制終了のシグナルを有効に戻します
-        signal.signal(signal.SIGTERM, signal.SIG_DFL)
-        signal.signal(signal.SIGINT, signal.SIG_DFL)
+    def on_finally(self):
+        # ここで終了処理
+        self.__diagram.clean_up()
+        return 1
 
 
 if __name__ == "__main__":
     """サンプルを実行します"""
-    sys.exit(__main())
+    sys.exit(MainFinally.run(Main()))
